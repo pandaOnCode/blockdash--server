f75c2651b5feaca341b480f30fe6bb118579c306
pandaOnCode <asing111@ucsc.edu>
2021-09-16 20:09:14 -0700

    deploy 5

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('hitting the backend');
+})
+
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 console.log(process.env.CONNECTION_URL)
